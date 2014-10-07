bc787dc17e00fa6d3e06af54333590122383dab6
Andrii Dvoiak <dvoyak@gmail.com>
2014-10-07 18:02:29 +0300

    fib2 function added

--- fib.py
+++ fib.py
@@ -17,9 +17,16 @@ def fib(x):
         return 1
     else:
         return fib(x-1)+fib(x-2)
+def fib2(x):
+    """Making a list of fib x fib numbers"""
+    l = [1,1]
+    while len(l)<x:
+        l.append(l[-1]+l[-2])
+    return l
 
 def main():
     print fib(7)
+    print fib2(7)
 
 if __name__ == '__main__':
     main()
