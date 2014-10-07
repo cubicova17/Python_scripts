6f4a659b8558f2833b66d20d3021bdd7b86eb49a
Andrii Dvoiak <dvoyak@gmail.com>
2014-10-07 18:27:22 +0300

    Added fib3 function, minor changes in fib and fib2 functions

--- fib.py
+++ fib.py
@@ -9,24 +9,36 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
+
 def fib(x):
-    """Fibonachi number"""
-    if x==1:
-        return 1
-    elif x==2:
+    """Recursive Fibonachi number"""
+    if x==0:
+        return 0
+    elif x==1:
         return 1
     else:
         return fib(x-1)+fib(x-2)
+
 def fib2(x):
-    """Making a list of fib x fib numbers"""
-    l = [1,1]
+    """Making a list of "x" fib numbers (no recursion)"""
+    l = [0,1]
     while len(l)<x:
         l.append(l[-1]+l[-2])
     return l
 
+def fib3(x):
+    """dfd"""
+    a = 0
+    b = 1
+    for i in range(x-2):
+        a,b = b,a+b
+    return b
+
 def main():
-    print fib(7)
-    print fib2(7)
+    N = 7
+    print fib(N)
+    print fib2(N)
+    print fib3(N)
 
 if __name__ == '__main__':
     main()
